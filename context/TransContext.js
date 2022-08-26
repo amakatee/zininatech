2f14a08c45102fe46f5558f8cd4104ec856d0c01
Ekaterina Zinina <ekaterinazinina@Ekaterinas-MacBook-Pro.local>
2022-08-26 19:11:29 +0300

    matrix slomo

--- context/TransContext.js
+++ context/TransContext.js
@@ -41,10 +41,10 @@ export const TransContext = ({children}) => {
             scrollTrigger: {
               trigger: entry.target,
             
-              start: '23%',
+              start: '30%',
 
               scrub:true,
-              end: '100%',
+              end: '130%',
               pin:true,
               pinSpacing: false,
     
@@ -65,7 +65,7 @@ export const TransContext = ({children}) => {
 
       useLayoutEffect(() => 
       {
-        gsap.fromTo(navItemRef.current, {x: -100, opacity:0} , {x:0, opacity:1, duration:.5, transition:'1s easeInOut', delay:.8})
+        gsap.fromTo(navItemRef.current, {x: -100, opacity:0} , {x:0, opacity:1, duration:.5, transition:'1s easeInOut', delay:.2})
 
 
       }, [])
@@ -77,10 +77,10 @@ export const TransContext = ({children}) => {
          gsap.set(matrixLine3('.matrix-letter '), {margin: "-20px"})
          gsap.set(matrixLine4('.matrix-letter '), {margin: "-20px"})
 
-         gsap.fromTo(matrixLine1('.matrix-letter'), {margin: "-20px"}, {margin: "0px", duration:1.2,  ease: Power3.easeInOut, delay:.3})
-         gsap.fromTo(matrixLine2('.matrix-letter'), {margin: "-20px"}, {margin: "-1px", duration:1.3,  ease: Power3.easeInOut, delay:.3})
-         gsap.fromTo(matrixLine3('.matrix-letter'),{margin: "-20px"}, {margin: "-2px", duration:1.9, ease: Power3.easeInOut, delay:.3})
-         gsap.fromTo(matrixLine4('.matrix-letter'),{margin: "-20px"}, {margin: "-1px", duration:1.4, ease: Power3.easeInOut, delay:.3})
+         gsap.fromTo(matrixLine1('.matrix-letter'), {margin: "-20px"}, {margin: "0px", duration:2,  ease: Power3.slowMo, delay:.3})
+         gsap.fromTo(matrixLine2('.matrix-letter'), {margin: "-20px"}, {margin: "-1px", duration:1.8,  ease: Power3.slowMo, delay:.3})
+         gsap.fromTo(matrixLine3('.matrix-letter'),{margin: "-20px"}, {margin: "-2px", duration:1.9, ease: Power3.slowMo, delay:.3})
+         gsap.fromTo(matrixLine4('.matrix-letter'),{margin: "-20px"}, {margin: "-1px", duration:1.4, ease: Power3.slowMo, delay:.3})
 
          gsap.fromTo(matrixLine1(".matrix-letter"), {y:0, x:0,opacity:0}, {y:-2,x:-4,opacity:1, stagger: .1,repeat:-1, duration: 1.1} )
          gsap.fromTo(matrixLine2(".matrix-letter"), {y:0,x: 0, opacity:0}, {y:3, x:-2,stagger: .1,opacity:1, repeat:-1,duration: 1.1})
